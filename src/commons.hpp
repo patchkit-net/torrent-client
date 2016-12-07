80c9fd967a8a519c17bb0bf7320cc7cf91f5e45b
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2016-12-07 21:53:14 +0100

    Fix reading paths with spaces

--- src/commons.hpp
+++ src/commons.hpp
@@ -5,8 +5,15 @@ static const char* SAVE_PATH = "downloads/content/";
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
     std::stringstream ss(s);
     std::string item;
+    std::string buf;
     while (std::getline(ss, item, delim)) {
-        elems.push_back(item);
+      if (item[item.length() - 1] == '\\') {
+        buf += item.substr(0, item.length() - 1) + delim;
+      } else {
+        buf += item;
+        elems.push_back(buf);
+        buf.clear();
+      }
     }
     return elems;
 }
